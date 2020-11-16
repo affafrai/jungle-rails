84043e749413954ad74c8234a84c76531b552a5a
affaf <affaf.rai@hotmail.com>
2020-11-16 18:54:24 +0000

    about page featured

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
-  resource :about, only: [:show]
-  root :to => "about#show"
+
+
+  resources :about, only: [:index]
+
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
